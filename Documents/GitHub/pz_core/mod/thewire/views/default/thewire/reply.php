05c613a78b5d203c344edbe96117443de69571e4
Planet Zuda, LLC <help@planetzuda.com>
2012-08-22 00:04:51 -0700

    Modified it so the access you start a conversation with should be the one used in a reply. Still isn't working.

--- Documents/GitHub/pz_core/mod/thewire/views/default/thewire/reply.php
+++ Documents/GitHub/pz_core/mod/thewire/views/default/thewire/reply.php
@@ -5,9 +5,11 @@
 
 $post = $vars['post'];
 $poster = $post->getOwnerEntity();
+$post_owner = get_entity(get_input('parent_guid'));
+			$poster->access_id = $access_id;
 $poster_details = array(
-	htmlspecialchars($poster->name,  ENT_QUOTES, 'UTF-8'),
-	htmlspecialchars($poster->username,  ENT_QUOTES, 'UTF-8'),
+	htmlentities($poster->name,  ENT_QUOTES, 'UTF-8'),
+	htmlentities($poster->username,  ENT_QUOTES, 'UTF-8'),
 );
 ?>
 <b><?php echo elgg_echo('thewire:replying', $poster_details); ?>: </b>
