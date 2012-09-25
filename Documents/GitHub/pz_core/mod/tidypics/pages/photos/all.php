ee2b934395f12dc0c1fb686ccd8593d1f829ee63
Planet Zuda, LLC <help@planetzuda.com>
2012-09-24 23:07:13 -0700

    fixed privacy issue, so everyone won't see your private photos.

--- Documents/GitHub/pz_core/mod/tidypics/pages/photos/all.php
+++ Documents/GitHub/pz_core/mod/tidypics/pages/photos/all.php
@@ -37,26 +37,3 @@ $body = elgg_view_layout('content', array(
 
 echo elgg_view_page($title, $body);
  */
-elgg_push_breadcrumb($owner->name, "photos/friends/$owner->username");
-elgg_push_breadcrumb(elgg_echo('friends'));
-
-$title = elgg_echo('album:friends');
-
-
-$num_albums = 16;
-
-set_input('list_type', 'gallery');
-$content = list_user_friends_objects($owner->guid, 'album', $num_albums, false);
-if (!$content) {
-	$content = elgg_echo('tidypics:none');
-}
-
-elgg_register_title_button();
-
-$body = elgg_view_layout('content', array(
-	'filter_context' => 'friends',
-	'content' => $content,
-	'title' => $title,
-));
-
-echo elgg_view_page($title, $body);
